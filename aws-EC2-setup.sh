64e249d1537cd2707ff39a9fe80b38c23d09b066
246002 <Ant0m0z!3>
2020-03-27 10:49:45 -0500

    clean project with bootstrap added

--- aws-EC2-setup.sh
+++ aws-EC2-setup.sh
@@ -3,4 +3,4 @@ sudo amazon-linux-extras install docker -y
 sudo service docker start
 # add ec2-user to docker group so you don't have to use sudo (requires restart of ec2 instance)
 #sudo usermod -a -G docker ec2-user
-sudo docker run --name angular-app-image-container -d -p 8080:80 aaronburk/angular-app-image:98060ba
+sudo docker run --name angular-app-image-container -d -p 8080:80 aaronburk/angular-app-image:830da2d
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
-      imports: [
-        FormsModule
       ]
     }).compileComponents();
   }));
@@ -19,31 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have default name 'Aaron'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.name).toEqual('Aaron');
-  });
-
-  it('should have input text type', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('input').type).toEqual('text');
-  });
-
-  it('should have the input field bound to name', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      const inputDebug = fixture.debugElement.query(By.css('input[type=text]'));
-      const input = inputDebug.nativeElement;
-      input.value = 'abc';
-      input.dispatchEvent(new Event('input'));
-      fixture.detectChanges();
-      expect(fixture.nativeElement.querySelector('p').textContent).toContain('abc');
-      });
   });
 });
