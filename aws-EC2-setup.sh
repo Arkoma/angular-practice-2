23ab735393ac9b594cbd074819b41f30facef104
246002 <Ant0m0z!3>
2020-03-31 16:43:59 -0500

    implemented simple router without using angular router

--- aws-EC2-setup.sh
+++ aws-EC2-setup.sh
@@ -1,4 +1,4 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
-sudo docker run --name angular-app-image-container -d -p 8080:80 aaronburk/angular-app-image:bccc815
+sudo docker run --name angular-app-image-container -d -p 8080:80 aaronburk/angular-app-image:c3112f1
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  mainContent = 'recipes';
+
+  setMainContent(content: string) {
+    this.mainContent = content;
+  }
 
 }
