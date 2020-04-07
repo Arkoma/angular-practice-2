265b22ae4983c48d619fc4131fbec35adf7ff0ba
246002 <Ant0m0z!3>
2020-04-07 15:08:16 -0500

    add version to aws EC2 docker file

--- aws-EC2-setup.sh
+++ aws-EC2-setup.sh
@@ -1,4 +1,4 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
-sudo docker run --name angular-app-image-container -d -p 8080:80 aaronburk/angular-app-image:54e1be1
+sudo docker run --name angular-app-image-container -d -p 8080:80 aaronburk/angular-app-image:a2b8ae5
